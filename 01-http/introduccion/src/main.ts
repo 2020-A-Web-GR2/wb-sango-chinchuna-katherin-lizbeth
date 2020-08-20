d96d597368969aa96573813c79e4320f5a1bbe9a
Katherin Sango <kate.liz40@gmail.com>
2020-08-20 17:06:56 -0500

    metodo almacenar usuario con mascota y configuracion de vistas

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';// Importar cosas en TS
 const cookieParser = require('cookie-parser');  // Importar cosas en JS
+const express = require('express');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as any;
   //npm run start:dev
   /*
   * AQUI CONFIGURACION
   * ANTES DEL APP.LISTE()
   * */
   app.use(cookieParser('My secret'));
+  app.set('view engine', 'ejs');
+  app.use(express.static('publico')) //poner servidor web estatico
+
+
   await app.listen(3001);
 }
 bootstrap();
--- 01-http/introduccion/src/usuario/usuario.module.ts
+++ 01-http/introduccion/src/usuario/usuario.module.ts
@@ -3,12 +3,14 @@ import {UsuarioController} from './usuario.controller';
 import {UsuarioService} from './usuario.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsuarioEntity} from "./usuario.entity";
+import {MascotaModule} from "./mascota/mascota.module";
 
 @Module({
     controllers: [
         UsuarioController
     ],
     imports: [
+        MascotaModule,
         TypeOrmModule
             .forFeature(
                 [
