b2e797275b03de440bed74f322a37111db71aa35
Katherin Sango <kate.liz40@gmail.com>
2020-09-11 19:02:38 -0500

    busquedas y configuracion de sesiones

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';// Importar cosas en TS
 const cookieParser = require('cookie-parser');  // Importar cosas en JS
 const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as any;
@@ -10,9 +12,19 @@ async function bootstrap() {
   * AQUI CONFIGURACION
   * ANTES DEL APP.LISTE()
   * */
-  app.use(cookieParser('My secret'));
-  app.set('view engine', 'ejs');
-  app.use(express.static('publico')) //poner servidor web estatico
+    app.use(cookieParser('My secret'));
+    app.set('view engine', 'ejs');
+    app.use(express.static('publico')) //poner servidor web estatico
+    app.use(
+        session({
+            name: 'server-session-id',
+            secret: 'No sera de tomar un traguito',
+            resave: true,
+            saveUninitialized: true,
+            cookie: {secure: false},
+            store: new FileStore(),
+        }),
+    );
 
 
   await app.listen(3001);
